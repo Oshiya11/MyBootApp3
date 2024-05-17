aaa203dd1b469922793fcfdc6711136bb76d673c
Oshiya11 <te210024@gmail.com>
2024-05-17 12:13:17 +0900

    最初の登録

---
+++ src/main/java/com/example/mybootapp3/HelloController.java
@@ -0,0 +1,14 @@
+package com.example.mybootapp3;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+/**
+* Hello world!
+*/
+@RestController
+public class HelloController
+{
+@RequestMapping("/taro")
+public String index() {
+return "Taro desu!";
+}
+}
